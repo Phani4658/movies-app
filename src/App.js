2bb4d5d8b935a14169c755847b67c9c762fef65e
Phani <mysteriousphani@gmail.com>
2024-01-12 19:25:41 +0000

    Add Protected Routes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import MovieDetails from './components/MovieDetails'
 import Account from './components/Account'
 import Search from './components/Search'
 import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
     <Route exact path="/login" component={Login} />
-    <Route exact path="/popular" component={PopularMovies} />
-    <Route exact path="/movies/:id" component={MovieDetails} />
-    <Route exact path="/search" component={Search} />
-    <Route exact path="/account" component={Account} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/popular" component={PopularMovies} />
+    <ProtectedRoute exact path="/movies/:id" component={MovieDetails} />
+    <ProtectedRoute exact path="/search" component={Search} />
+    <ProtectedRoute exact path="/account" component={Account} />
     <Route exact path="/not-found" component={NotFound} />
     <Redirect to="/not-found" />
   </Switch>
